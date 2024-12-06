6c457aabd502f9b9f4dcdfea109d53015a20a04a
GACHTOU NourEddine <gachtoun@gmail.com>
2024-12-06 21:59:57 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/', [\App\Http\Controllers\WecolmeController::class, 'index'])->name
 
 Route::get('/product/{productNameVariable}/{productPrice}', [\App\Http\Controllers\WecolmeController::class, 'product'])->name('product-page');
 
+// Define all possible routes for our shop, customer side not admin panel
+// create all possible migrations and define tables and columns : php artisan make:migration create_users_table
+// create factory for one table
